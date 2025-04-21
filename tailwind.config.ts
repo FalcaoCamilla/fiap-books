11a421044fb348d67df28d6875915d04defdc31b
Camilla Falcão <millafalcao.acad@gmail.com>
2025-04-21 20:45:36 -0300

    feat: add custom colors, font families, and font sizes to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        black: {
+          900: "#1E1E1E",
+          800: "#1B1E20",
+        },
+        pink: {
+          900: "#ED145B",
+          800: "#FF007A",
+        },
+      },
+      fontFamily: {
+        sans: ["Arial", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Menlo", "monospace"],
+      },
+      fontSize: {
+        xs: ["0.75rem", { lineHeight: "1rem" }],
+        sm: ["0.875rem", { lineHeight: "1.25rem" }],
+        base: ["1rem", { lineHeight: "1.5rem" }],
+        lg: ["1.125rem", { lineHeight: "1.75rem" }],
+        xl: ["1.25rem", { lineHeight: "1.75rem" }],
+        "2xl": ["1.5rem", { lineHeight: "2rem" }],
+        "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+        "4xl": ["2.25rem", { lineHeight: "2.5rem" }], // 36px
+        "5xl": ["3rem", { lineHeight: "1" }],
+      },
     },
   },
   plugins: [],
